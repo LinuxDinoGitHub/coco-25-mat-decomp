b4a2cd2a3077b169c227834de0aa199fdd86d41b
Rufus LEE <leec150@Rufus-Lee.local>
2025-09-20 22:50:19 +0800

    react is stupid

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <SpeedInsights/>
-      
+      <SpeedInsights />
+      <main>
+        <p>React is BumFuck Stupid</p>
+      </main>
     </div>
   );
 }
